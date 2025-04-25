a4b9b765b969300c9e112f5c2ea6f13e25d2e5ad
duonyyy <duongtungduong34@gmail.com>
2025-04-25 23:43:08 +0700

    Add console print to searchByProductId

--- src/OrderManager.java
+++ src/OrderManager.java
@@ -1,12 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.logging.Logger;
-import java.util.logging.Level;
 
 public class OrderManager {
     private List<Order> orders;
-    private static final Logger LOGGER = Logger.getLogger(OrderManager.class.getName());
 
     public OrderManager() {
         this.orders = new ArrayList<>();
@@ -21,7 +18,7 @@ public class OrderManager {
     }
 
     public List<Order> searchByProductId(String productId) {
-        LOGGER.log(Level.INFO, "Searching orders for productId: {0}", productId);
+        System.out.println("Searching for product: " + productId);
         return orders.stream()
                 .filter(order -> order.getProductId().equals(productId))
                 .collect(Collectors.toList());
