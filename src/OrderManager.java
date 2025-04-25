e94096b1212b872df56b1cb8ed69f355657e287a
duonyyy <duongtungduong34@gmail.com>
2025-04-25 23:41:48 +0700

    Add logging to searchByProductId

--- src/OrderManager.java
+++ src/OrderManager.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class OrderManager {
     private List<Order> orders;
+    private static final Logger LOGGER = Logger.getLogger(OrderManager.class.getName());
 
     public OrderManager() {
         this.orders = new ArrayList<>();
@@ -18,6 +21,7 @@ public class OrderManager {
     }
 
     public List<Order> searchByProductId(String productId) {
+        LOGGER.log(Level.INFO, "Searching orders for productId: {0}", productId);
         return orders.stream()
                 .filter(order -> order.getProductId().equals(productId))
                 .collect(Collectors.toList());
